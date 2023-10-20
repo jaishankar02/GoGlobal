cefa91e318b58e93f4ae2d4c0ae78ce3347ad7d6
Niharika Sihag <nikki25choudhary@gmail.com>
2023-10-20 23:57:59 +0530

    updated login and signup

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,90 +1,106 @@
-import React from 'react'
-
+import React from "react";
+import "../assets/css/login.css";
 const Login = () => {
   return (
-    <>
-    <form>
-    <>
-  <div className="input-group mb-3">
-    <span className="input-group-text" id="basic-addon1">
-      @
-    </span>
-    <input
-      type="text"
-      className="form-control"
-      placeholder="Username"
-      aria-label="Username"
-      aria-describedby="basic-addon1"
-    />
-  </div>
-  <div className="input-group mb-3">
-    <input
-      type="text"
-      className="form-control"
-      placeholder="Recipient's username"
-      aria-label="Recipient's username"
-      aria-describedby="basic-addon2"
-    />
-    <span className="input-group-text" id="basic-addon2">
-      @example.com
-    </span>
-  </div>
-  <div className="mb-3">
-    <label htmlFor="basic-url" className="form-label">
-      Your vanity URL
-    </label>
-    <div className="input-group">
-      <span className="input-group-text" id="basic-addon3">
-        https://example.com/users/
-      </span>
-      <input
-        type="text"
-        className="form-control"
-        id="basic-url"
-        aria-describedby="basic-addon3 basic-addon4"
-      />
-    </div>
-    <div className="form-text" id="basic-addon4">
-      Example help text goes outside the input group.
-    </div>
-  </div>
-  <div className="input-group mb-3">
-    <span className="input-group-text">$</span>
-    <input
-      type="text"
-      className="form-control"
-      aria-label="Amount (to the nearest dollar)"
-    />
-    <span className="input-group-text">.00</span>
-  </div>
-  <div className="input-group mb-3">
-    <input
-      type="text"
-      className="form-control"
-      placeholder="Username"
-      aria-label="Username"
-    />
-    <span className="input-group-text">@</span>
-    <input
-      type="text"
-      className="form-control"
-      placeholder="Server"
-      aria-label="Server"
-    />
-  </div>
-  <div className="input-group">
-    <span className="input-group-text">With textarea</span>
-    <textarea
-      className="form-control"
-      aria-label="With textarea"
-      defaultValue={""}
-    />
-  </div>
-</>
+    <section className="p-0  my-element-area font-family">
+      <div className="row gx-0  ">
+        <h1 className="text-center">
+          You can start Buying once you complete this
+        </h1>
+        <div className="col-lg-7 col-md-12 mt-lg-0 mt-5 align-self-center">
+          {/* <h1
+          style={{ color: "#033063" }}
+          className="my-heading our-hero-heading mb-5 text-center"
+        >
+          Join the tribe of over 10,000+{" "}
+          <span className="boujee-text">Teenagers</span>{" "}
+        </h1> */}
+          {/* <h1
+          style={{ color: "#033063" }}
+          className="my-heading  our-hero-heading mb-4 text-center"
+        >
+          Get <span className="boujee-text"> HeadStart </span> Today!{" "}
+        </h1> */}
+          {/* <h3 class="text-center fw-bolder mb-5" style="color: #033063;">Hear what they have to say!</h3> */}
+          {/* <div class="p-0"> */}
+          {/* <div class="row ">
+               
+            <div class="col-lg-12">
+                <div class="row gx-lg-5   mx-auto justify-content-around ">
+                <div class="col-lg-4   mt-lg-0 mt-5">
+          
+               <video src="images/testimonial_aditya_video.mp4" controls autoplay width="100%" ></video>
+                </div>
+                <div class="col-lg-4     mt-lg-0 mt-5">
+          
+                    <video src="images/testimonial_aditya_video.mp4" controls autoplay width="100%" ></video>
+                     </div>
+                     <div class="col-lg-4 text-center  mt-lg-0 mt-5">
+          
+                        <video src="images/testimonial_aditya_video.mp4" controls autoplay width="100%" ></video>
+                         </div>
+            
+                </div>
+            </div>
+            </div> */}
+          {/* </div> */}
+        </div>
+        <div className="col-lg-5 ">
+          <div className="   my-form-start mb-lg-0 mb-5 ">
+            <div className="form-section   col-lg-12 justify-content-center card-body text-start">
+              <h2 className="form-heading text-black text-start mt-5 fw-bold">
+                Log in to Go Global
+              </h2>
+              <h6 className="form-heading text-black text-start mt-4 fw-bold">
+                Enter your details below
+              </h6>
+              <form className="mt-5 my-form">
+                <div className="form-row ">
+                  <label htmlFor="email" />
+                  <input
+                    className="text-start float-start"
+                    type="email"
+                    id="email"
+                    placeholder="Email or Phone Number"
+                    name="email"
+                    required=""
+                  />
+                </div>
+                <div className="form-row mt-5  text-black">
+                  <label className="border-bottom" htmlFor="student-name" />
+                  <input
+                    className="text-start float-start"
+                    type="text"
+                    id="password"
+                    placeholder="Password"
+                    name="password"
+                    required=""
+                  />
+                </div>
+                <div className="form-row  mt-4  text-black">
+                
+                  <button
+                    
+                    type="submit"
+                    className="btn d-inline-block my-login-btn my-faq-text   shadow my-button-login text-white rounded-3 py-3 mt-4 btn-sm "
+                  >
+                    Login
+                  </button>
 
-    </form>
-    </>
-  )
-}
+                 <button className="btn d-inline-block  my-faq-text    my-button text-white rounded-3 py-3  mt-4 btn-sm ">
+                 <h6 className="d-inline-block mx-5 text-black">Forgot Password?</h6>
+                 </button>
+                 
+                </div>
+                
+                
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default Login
+export default Login;
--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,10 +1,129 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import "../assets/css/signup.css";
 const SignUp = () => {
+    const [name,setName]=useState('');
+    const [email,setEmail]=useState('');
+    const [password,setPassword]=useState('');
     return (
-        <>
-        </>
-    )
+        <section className="p-0  my-element-area-signup font-family">
+          <div className="row gx-0  ">
+            <h1 className="text-center">
+              You can start Buying once you complete this
+            </h1>
+            <div className="col-lg-7 col-md-12 mt-lg-0 mt-5 align-self-center">
+              {/* <h1
+              style={{ color: "#033063" }}
+              className="my-heading our-hero-heading mb-5 text-center"
+            >
+              Join the tribe of over 10,000+{" "}
+              <span className="boujee-text">Teenagers</span>{" "}
+            </h1> */}
+              {/* <h1
+              style={{ color: "#033063" }}
+              className="my-heading  our-hero-heading mb-4 text-center"
+            >
+              Get <span className="boujee-text"> HeadStart </span> Today!{" "}
+            </h1> */}
+              {/* <h3 class="text-center fw-bolder mb-5" style="color: #033063;">Hear what they have to say!</h3> */}
+              {/* <div class="p-0"> */}
+              {/* <div class="row ">
+                   
+                <div class="col-lg-12">
+                    <div class="row gx-lg-5   mx-auto justify-content-around ">
+                    <div class="col-lg-4   mt-lg-0 mt-5">
+              
+                   <video src="images/testimonial_aditya_video.mp4" controls autoplay width="100%" ></video>
+                    </div>
+                    <div class="col-lg-4     mt-lg-0 mt-5">
+              
+                        <video src="images/testimonial_aditya_video.mp4" controls autoplay width="100%" ></video>
+                         </div>
+                         <div class="col-lg-4 text-center  mt-lg-0 mt-5">
+              
+                            <video src="images/testimonial_aditya_video.mp4" controls autoplay width="100%" ></video>
+                             </div>
+                
+                    </div>
+                </div>
+                </div> */}
+              {/* </div> */}
+            </div>
+            <div className="col-lg-5 ">
+              <div className="   my-form-start-signup mb-lg-0 mb-5 ">
+                <div className="form-section   col-lg-12 justify-content-center card-body text-start">
+                  <h2 className="form-heading text-black text-start mt-1 fw-bold">Create Your Account
+                  </h2>
+                  <h6 className="form-heading text-black text-start mt-4 fw-bold">
+                    Enter your details below
+                  </h6>
+                  <form className="mt-4 my-form">
+                  <div className="form-row ">
+                      <label htmlFor="email" />
+                      <input
+                        className="text-start float-start"
+                        type="Name"
+                        id="Name"
+                        placeholder="Name"
+                        name="Name"
+                        required=""
+                      />
+                    </div>
+                    <div className="form-row mt-5 ">
+                      <label htmlFor="email" />
+                      <input
+                        className="text-start float-start"
+                        type="email"
+                        id="email"
+                        placeholder="Email or Phone Number"
+                        name="email"
+                        required=""
+                      />
+                    </div>
+                    <div className="form-row mt-5  text-black">
+                      <label className="border-bottom" htmlFor="student-name" />
+                      <input
+                        className="text-start float-start"
+                        type="text"
+                        id="password"
+                        placeholder="Password"
+                        name="password"
+                        required=""
+                      />
+                    </div>
+                    <div className="mt-4   text-black">
+                    
+                      <button
+                        type="submit"
+                        className="btn d-inline-block  my-faq-text  my-signup-btn  shadow my-button text-white rounded-3 py-3 mt-4 btn-sm "
+                      >
+                        Create Account
+                      </button>
+    
+                     
+                     
+                    </div>
+                    <div className="form-row  text-black">
+                    
+                      <button
+                        
+                        type="submit"
+                        className="btn d-inline-block  my-faq-text  my-google-signup-btn  shadow my-button text-white rounded-3 py-3 mt-4 btn-sm "
+                      >
+                        Sign up with Google
+                      </button>
+    
+                     
+                     
+                    </div>
+                    
+                    
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      );
 }
 
 export default SignUp
