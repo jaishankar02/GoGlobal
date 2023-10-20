ca996912ca6d16ae145a787e8e4cc1c20bbf6747
JaiShankar <jaishankar.a1603@gmail.com>
2023-10-20 21:02:58 +0530

    installed few more packages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
-import './App.css';
 
+import './App.css';
+import { BrowserRouter as Router, Routes, Route, ScrollRestoration, createRoutesFromElements, createBrowserRouter, RouterProvider } from 'react-router-dom';
+const RouteJSX = (
+  <>
+    <Route index element={<Button />} />
+  </>
+);
+const routes = createRoutesFromElements(RouteJSX);
+const router = createBrowserRouter(routes);
 function App() {
   return (
-    <></>
+    <>
+      <RouterProvider router={router} />
+
+    </>
   );
 }
 
